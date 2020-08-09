5da66efc87ee70b40039422f38d65ba078769979
sangrim-96 <56021431+sangrim-96@users.noreply.github.com>
2020-08-09 11:55:58 +0900

    5. Echo

--- main.go
+++ main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"strings"
 
-	"github.com/sangrimlee/go-scrapper/banking"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/sangrimlee/go-scrapper/scrapper"
 )
 
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
+	term := strings.ToLower(scrapper.Strip(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
+}
+
 func main() {
-	account := banking.NewAccount("Sang Rim Lee")
-	account.Deposit(10000)
-	fmt.Println(account.Owner(), account.Balance())
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
 }
